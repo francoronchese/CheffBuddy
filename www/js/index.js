5f2fc13d9086b1742a6a1d7d76833fbf3d0584c8
AucanFurioso <maximrosescout@gmail.com>
2024-11-02 20:23:27 -0300

    camara

--- www/js/index.js
+++ www/js/index.js
@@ -1,13 +1,3 @@
-
-/*document.addEventListener('deviceready', onDeviceReady, false);
-
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-}*/
-
 //Ventana Modal de Cámara
 const openModal = document.getElementById('open-button');
 const closeModal = document.getElementById('close-button');
@@ -20,6 +10,30 @@ openModal.addEventListener('click', () =>{
 closeModal.addEventListener('click', () =>{
     modal.close();
 })
+
+// camara
+
+// Botón para tomar foto
+document.getElementById("btnCamera").addEventListener("click", function () {
+    // Usa la API de la cámara para tomar una foto
+    navigator.camera.getPicture(onSuccess, onFail, {
+      quality: 50, // Calidad de la imagen
+      destinationType: Camera.DestinationType.DATA_URL, // Tipo de destino: base64
+    });
+  
+    // Función que se ejecuta si la foto se toma correctamente
+    function onSuccess(imageData) {
+      let image = document.getElementById("myImage"); // Obtiene la referencia del elemento img
+      image.src = "data:image/jpeg;base64," + imageData; // Asigna la imagen tomada en formato base64
+      image.style.display = "block"; // Muestra la imagen en el HTML
+    }
+  
+    // Función que se ejecuta si ocurre un error al tomar la foto
+    function onFail(message) {
+      alert("Error al tomar la foto: " + message); // Muestra un mensaje de error en un alert
+    }
+  });
+  
 
 //Búsqueda de Recetas
 
