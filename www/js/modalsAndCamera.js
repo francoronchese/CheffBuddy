5de9d75e21338d50c06be1e0d5744d66bed10a1d
francoronchese <lucasronchese44@gmail.com>
2024-11-04 21:28:06 -0300

    API y ventana de Receta(Incompleta)

--- www/js/modalsAndCamera.js
+++ www/js/modalsAndCamera.js
@@ -1,38 +1,39 @@
-//Búsqueda de Recetas
+//Ventana Modal de Cámara
+const openModal = document.getElementById('open-button');
+const closeModal = document.getElementById('close-button');
+const modal = document.getElementById('modal');
 
-const searchButton = document.getElementById('search-btn'); // Icono de búsqueda
-const mainTitle = document.querySelector('.main-title');    // H2 del título
-const productWrapper = document.querySelector('.product-wrapper');  // Contenedor de recetas
-const searchSection = document.querySelector('.search-section');    // Sección de búsqueda
+openModal.addEventListener('click', () =>{
+    modal.showModal();
+})
 
-searchButton.addEventListener('click', () => {
-    // Cambiar el título a "Search"
-    mainTitle.textContent = 'Buscar Recetas';
+closeModal.addEventListener('click', () =>{
+    modal.close();
+})
 
-    // Ocultar las recetas y mostrar la barra de búsqueda
-    productWrapper.classList.add('hidden');
-    searchSection.style.display = 'block';
-});
-
-
-/*Todas las recetas*/
-
-const homeButton = document.getElementById('home-btn');
-
-homeButton.addEventListener('click', () => {
-    mainTitle.textContent = 'Todas las Recetas'; // Cambiar el título de nuevo a 'Todas las Recetas'
-    
-    // Mostrar las recetas y ocultar la sección de búsqueda
-    productWrapper.classList.remove('hidden');
-    searchSection.style.display = 'none';
-});
-
-
-
-
+// Botón para tomar foto
+document.getElementById("btnCamera").addEventListener("click", function () {
+    // Usa la API de la cámara para tomar una foto
+    navigator.camera.getPicture(onSuccess, onFail, {
+      quality: 50, // Calidad de la imagen
+      destinationType: Camera.DestinationType.DATA_URL, // Tipo de destino: base64
+    });
+  
+    // Función que se ejecuta si la foto se toma correctamente
+    function onSuccess(imageData) {
+      let image = document.getElementById("myImage"); // Obtiene la referencia del elemento img
+      image.src = "data:image/jpeg;base64," + imageData; // Asigna la imagen tomada en formato base64
+      image.style.display = "block"; // Muestra la imagen en el HTML
+    }
+  
+    // Función que se ejecuta si ocurre un error al tomar la foto
+    function onFail(message) {
+      alert("Error al tomar la foto: " + message); // Muestra un mensaje de error en un alert
+    }
+  });
 
 
-// Modal para mostrar los ingredientes
+  // Modal para mostrar los ingredientes
 const modalIngredients = document.getElementById('modal-ingredients');
 const closeIngredientsModal = document.getElementById('close-ingredients-modal');
 const ingredientsList = document.getElementById('ingredients-list'); // Asegúrate de tener un elemento con este ID en tu HTML
